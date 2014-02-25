40eeee3683acb3046d3a6bab1918bc3305601f64
RobertLutken <rolandroid117@gmail.com>
2014-02-25 00:51:27 +0000

    Previous and Cont Buttons Completed & Wallpaper is Updated, Actions for hover, mouse in , mouse out and mouse click handled

--- CIP/Form1.Designer.cs
+++ CIP/Form1.Designer.cs
@@ -28,45 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnContinue = new System.Windows.Forms.Button();
+            this.btnPrevious = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btnContinue
             // 
-            this.button1.BackColor = System.Drawing.Color.Transparent;
-            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.ForeColor = System.Drawing.Color.Transparent;
-            this.button1.Image = global::CIP.Properties.Resources.ArrowLeft;
-            this.button1.Location = new System.Drawing.Point(978, -10);
-            this.button1.Name = "button1";
-            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.button1.Size = new System.Drawing.Size(115, 842);
-            this.button1.TabIndex = 0;
-            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
-            this.button1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button1_MouseDown);
-            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
-            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            this.btnContinue.AutoSize = true;
+            this.btnContinue.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnContinue.BackColor = System.Drawing.Color.Transparent;
+            this.btnContinue.BackgroundImage = global::CIP.Properties.Resources.ArrowLeft;
+            this.btnContinue.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnContinue.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnContinue.FlatAppearance.BorderSize = 0;
+            this.btnContinue.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnContinue.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnContinue.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnContinue.Font = new System.Drawing.Font("Microsoft Sans Serif", 1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnContinue.ForeColor = System.Drawing.Color.Transparent;
+            this.btnContinue.Image = global::CIP.Properties.Resources.ArrowLeft;
+            this.btnContinue.Location = new System.Drawing.Point(940, 0);
+            this.btnContinue.Name = "btnContinue";
+            this.btnContinue.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnContinue.Size = new System.Drawing.Size(172, 829);
+            this.btnContinue.TabIndex = 0;
+            this.btnContinue.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnContinue.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnContinue.UseVisualStyleBackColor = true;
+            this.btnContinue.Click += new System.EventHandler(this.button1_Click);
+            this.btnContinue.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
+            this.btnContinue.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button1_MouseDown);
+            this.btnContinue.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            this.btnContinue.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            this.btnContinue.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button1_MouseUp);
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.AutoSize = true;
+            this.btnPrevious.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnPrevious.BackColor = System.Drawing.Color.Transparent;
+            this.btnPrevious.BackgroundImage = global::CIP.Properties.Resources.gradient_background_wallpaper_u___ibackgroundz_com;
+            this.btnPrevious.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnPrevious.FlatAppearance.BorderSize = 0;
+            this.btnPrevious.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnPrevious.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnPrevious.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPrevious.Font = new System.Drawing.Font("Microsoft Sans Serif", 1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrevious.ForeColor = System.Drawing.Color.Transparent;
+            this.btnPrevious.Image = global::CIP.Properties.Resources.ArrowRight;
+            this.btnPrevious.Location = new System.Drawing.Point(0, 0);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnPrevious.Size = new System.Drawing.Size(172, 829);
+            this.btnPrevious.TabIndex = 1;
+            this.btnPrevious.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnPrevious.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnPrevious.UseVisualStyleBackColor = false;
+            this.btnPrevious.Click += new System.EventHandler(this.button2_Click);
+            this.btnPrevious.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button2_MouseClick);
+            this.btnPrevious.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button2_MouseDown);
+            this.btnPrevious.MouseEnter += new System.EventHandler(this.button2_MouseEnter);
+            this.btnPrevious.MouseLeave += new System.EventHandler(this.button2_MouseLeave);
+            this.btnPrevious.MouseHover += new System.EventHandler(this.button2_MouseHover);
+            this.btnPrevious.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button2_MouseUp);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackgroundImage = global::CIP.Properties.Resources.gradient_background_wallpaper_u___ibackgroundz_com;
             this.ClientSize = new System.Drawing.Size(1112, 829);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.btnContinue);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnContinue;
+        private System.Windows.Forms.Button btnPrevious;
     }
 }
 
--- CIP/Form1.cs
+++ CIP/Form1.cs
@@ -23,30 +23,72 @@ namespace CIP
         }
         private void button1_OnMouseEnter(object sender, EventArgs e)
         {
-            button1.Image = Image.FromFile(@"U:\Computing Individual Project\Code Base\ComputingProject\CIP\Resources\ArrowLeftClick.png");
-         
+          //  button1.Image = Image.FromFile(@"U:\Computing Individual Project\Code Base\ComputingProject\CIP\Resources\ArrowLeftHover.png");
+            
         }
 
         private void button1_MouseClick(object sender, MouseEventArgs e)
         {
-            button1.Image = Image.FromFile(@"U:\Computing Individual Project\Code Base\ComputingProject\CIP\Resources\ArrowLeftClick.png");
+            btnContinue.Image = Image.FromFile(@"U:\Computing Individual Project\Code Base\ComputingProject\CIP\Resources\ArrowLeftClick.png");
       
         }
 
         private void button1_MouseDown(object sender, MouseEventArgs e)
         {
-            button1.Image = Image.FromFile(@"U:\Computing Individual Project\Code Base\ComputingProject\CIP\Resources\ArrowLeftClick.png");
+          btnContinue.Image = Image.FromFile(@"U:\Computing Individual Project\Code Base\ComputingProject\CIP\Resources\ArrowLeftClick.png");
           
         }
 
         private void button1_MouseLeave(object sender, EventArgs e)
         {
-            button1.Image = Image.FromFile(@"U:\Computing Individual Project\Code Base\ComputingProject\CIP\Resources\ArrowLeft.png");
+            btnContinue.Image = Image.FromFile(@"U:\Computing Individual Project\Code Base\ComputingProject\CIP\Resources\ArrowLeft.png");
         }
 
         private void button1_MouseEnter(object sender, EventArgs e)
         {
-            button1.Image = Image.FromFile(@"U:\Computing Individual Project\Code Base\ComputingProject\CIP\Resources\ArrowLeftEnter.png");
+            btnContinue.Image = Image.FromFile(@"U:\Computing Individual Project\Code Base\ComputingProject\CIP\Resources\ArrowLeftHover.png");
+        }
+
+        private void button1_MouseUp(object sender, MouseEventArgs e)
+        {
+            btnContinue.Image = Image.FromFile(@"U:\Computing Individual Project\Code Base\ComputingProject\CIP\Resources\ArrowLeftHover.png");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_MouseClick(object sender, MouseEventArgs e)
+        {
+            btnPrevious.Image = Image.FromFile(@"U:\Computing Individual Project\Code Base\ComputingProject\CIP\Resources\ArrowRightClick.png");
+        }
+
+        private void button2_MouseEnter(object sender, EventArgs e)
+        {
+            btnPrevious.Image = Image.FromFile(@"U:\Computing Individual Project\Code Base\ComputingProject\CIP\Resources\ArrowRightHover.png");
+        }
+
+        private void button2_MouseHover(object sender, EventArgs e)
+        {
+            btnPrevious.Image = Image.FromFile(@"U:\Computing Individual Project\Code Base\ComputingProject\CIP\Resources\ArrowRightHover.png");
+        }
+
+        private void button2_MouseUp(object sender, MouseEventArgs e)
+        {
+            // Reset to Hover 
+            btnPrevious.Image = Image.FromFile(@"U:\Computing Individual Project\Code Base\ComputingProject\CIP\Resources\ArrowRightHover.png");
+        }
+
+        private void button2_MouseLeave(object sender, EventArgs e)
+        {
+            // Reset to default state
+            btnPrevious.Image = Image.FromFile(@"U:\Computing Individual Project\Code Base\ComputingProject\CIP\Resources\ArrowRight.png");
+        }
+
+        private void button2_MouseDown(object sender, MouseEventArgs e)
+        {
+            btnPrevious.Image = Image.FromFile(@"U:\Computing Individual Project\Code Base\ComputingProject\CIP\Resources\ArrowRightClick.png");
         }
 
 
